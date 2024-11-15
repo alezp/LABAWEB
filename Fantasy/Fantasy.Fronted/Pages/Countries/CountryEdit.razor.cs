a633ac196ef4d1c74d8b5c82779c4faf3a4553cf
alezp <polli.alejandro89@gmail.com>
2024-11-15 11:15:52 -0300

    Add Literals y Data Anotations

--- Fantasy/Fantasy.Fronted/Pages/Countries/CountryEdit.razor.cs
+++ Fantasy/Fantasy.Fronted/Pages/Countries/CountryEdit.razor.cs
@@ -32,7 +32,8 @@ public partial class CountryEdit
             else
             {
                 var messageError = await responseHttp.GetErrorMessageAsync();
-                await SweetAlertService.FireAsync(Localizer["Error"], messageError, SweetAlertIcon.Error);
+                await SweetAlertService.FireAsync(Localizer["Error"], Localizer[messageError!], SweetAlertIcon.Error);
+
             }
         }
         else
