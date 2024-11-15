c33c8c2c04080256c1fc9dfe74ecb3deef05df59
alezp <polli.alejandro89@gmail.com>
2024-11-15 11:05:06 -0300

    Add Teams Controller and Repositories and Implementations

--- Fantasy/Fantasy.Shared/Responses/ActionResponse.cs
+++ Fantasy/Fantasy.Shared/Responses/ActionResponse.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Fantasy.Shared.Responses;
 
-namespace Fantasy.Shared.Responses
+public class ActionResponse<T>
 {
-    internal class ActionResponse
-    {
-    }
+    public bool WasSuccess { get; set; }
+
+    public string? Message { get; set; }
+
+    public T? Result { get; set; }
 }
